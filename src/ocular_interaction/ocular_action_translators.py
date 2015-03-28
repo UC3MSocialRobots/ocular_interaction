532d9a47432a5b28aa336aed2ca9e76856d4eb67
Victor Gonzalez <victor.gonzalez.pacheco@gmail.com>
2015-03-28 13:40:43 +0100

    action_translators.py: now uses a function to parse action_args and return them as a dict

--- src/ocular_interaction/ocular_action_translators.py
+++ src/ocular_interaction/ocular_action_translators.py
@@ -12,11 +12,17 @@ roslib.load_manifest('ocular_interaction')
 from ocular.msg import EventHandler
 
 
+def action_args_to_dict(action):
+    """ Return action.args in form of a dictionary. """
+    if not action.args:
+        return None
+    d = {arg.name: arg.value for arg in action.args}
+    return d
+
+
 def action_to_event_handler(action):
     """ Convert a dialog_manager_msgs/ActionMsg to ocular/EventHandler.msg."""
-    if not action.args:
-        return EventHandler()
-    d = {arg.name: arg.value for arg in action.args}
+    d = action_args_to_dict(action)
     return EventHandler(hand=d.get('hand', None),
                         event=d.get('event', None),
                         last_event=d.get('last_event', None))
