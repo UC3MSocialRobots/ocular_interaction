4e8bd046cf8bb7eef10a2e6d2e0e8611f4612218
monarch <monarch@mpc01.(none)>
2015-04-01 11:16:19 +0200

    [FEAT] action_translators now has a func to validate an action
    I've added a predicate function that parses an incoming action to validate
    this action against an actor and an action name.
    If the fields match the predicate returns True. Otherwise False.
    
     adding a predicate to parse wether an action translator should

--- ocular_interaction/src/ocular_interaction/ocular_action_translators.py
+++ ocular_interaction/src/ocular_interaction/ocular_action_translators.py
@@ -8,8 +8,32 @@ This module provides Action Translators for OCULAR messages
 
 import roslib
 roslib.load_manifest('ocular_interaction')
-
+from rospy import logdebug, logwarn
 from ocular.msg import EventHandler
+
+
+def should_process_action(action_msg, actor_name, action_name):
+    """
+    Check if an action should be processed by an action translator.
+
+    :param action_msg: The action message to be checked
+    :param actor_name: Name of the expected actor checked against action.actor
+    :param action_name: Name of the expected action checked against action.name
+    :return: True if action.name and action.actor are the expected ones
+    :rtype: bool
+    """
+    if action_msg.name != action_name:
+        logdebug("DM Logger Node: Action name is {}. I Only process action: {}"
+                 .format(action_msg.name, action_name))
+        return False
+    if action_msg.actor != actor_name:
+        logdebug("DM Logger Node: action {} is for actor {}. Discarding"
+                 .format(action_msg.name, action_msg.actor))
+        return False
+    if not action_msg.args:
+        logdebug("DM Logger Node: Action Message has empty args.")
+        return False
+    return True
 
 
 def action_args_to_dict(action):
@@ -23,6 +47,7 @@ def action_args_to_dict(action):
 def action_to_event_handler(action):
     """ Convert a dialog_manager_msgs/ActionMsg to ocular/EventHandler.msg."""
     d = action_args_to_dict(action)
+    logwarn("Received event from iwaki: %s", d)
     return EventHandler(hand=d.get('hand', None),
                         event=d.get('event', None),
                         last_event=d.get('last_event', None))
