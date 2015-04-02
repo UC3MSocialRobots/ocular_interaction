3320ab01ff01edccd98875678d2bb61247153570
monarch <victor.gonzalez.pacheco@gmail.com>
2015-04-02 11:24:07 +0200

    Improving docstring formatting

--- ocular_interaction/src/ocular_interaction/utils.py
+++ ocular_interaction/src/ocular_interaction/utils.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+# :license       LASR_UC3M v1.0, ver LICENCIA.txt
+
+# Este programa es software libre: puede redistribuirlo y/o modificarlo
+# bajo los terminos de la Licencia Academica Social Robotics Lab - UC3M
+# publicada por la Universidad Carlos III de Madrid, tanto en su version 1.0
+# como en una version posterior.
+
+# Este programa se distribuye con la intencion de que sea util,
+# pero SIN NINGUNA GARANTIA. Para mas detalles, consulte la
+# Licencia Academica Social Robotics Lab - UC3M version 1.0 o posterior.
+
+# Usted ha recibido una copia de la Licencia Academica Social
+# Robotics Lab - UC3M en el fichero LICENCIA.txt, que tambien se encuentra
+# disponible en <URL a la LASR_UC3Mv1.0>.
+
+"""
+Several utilities for the OCULAR Interaction packages.
+
+:author: Victor Gonzalez Pacheco
+:maintainer: Victor Gonzalez Pacheco
+"""
+
 import roslib
 roslib.load_manifest('monarch_multimodal_fusion')
 # import rospy
@@ -13,7 +35,6 @@ def_slots = {'timestamp', 'consumed', 'subtype', 'type'}
 def log_atom(msg, logger=loginfo):
     """
     Log an AtomMsg.
-
 
     :param msg AtomMsg: The message to send to logger
     :param logger: The logger function. Default :py:func:rospy.loginfo
