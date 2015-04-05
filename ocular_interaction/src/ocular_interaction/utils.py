66908cdc0d0d52dbdcd36e0acba8c3321708ade6
monarch <victor.gonzalez.pacheco@gmail.com>
2015-04-05 14:56:52 +0200

    utils.py: Added colorize functions and now log_atom colorizes its log messages

--- ocular_interaction/src/ocular_interaction/utils.py
+++ ocular_interaction/src/ocular_interaction/utils.py
@@ -29,38 +29,54 @@ from pattern.es import parse
 from enum import Enum
 from rospy import loginfo
 from functools import partial
+import colorama
+
 
 # LOGGING UTILS ###############################################################
 def_slots = {'timestamp', 'consumed', 'subtype', 'type'}
 
 
-def log_atom(msg, logger=loginfo):
+def colorize(logmsg, color=colorama.Fore.MAGENTA):
+    """Colorize a message with green colors."""
+    return ''.join([color, logmsg, colorama.Fore.RESET])
+
+blue = partial(colorize, color=colorama.Fore.BLUE)
+gree = partial(colorize, color=colorama.Fore.GREEN)
+
+
+def log_atom(msg, logger=loginfo, color=blue):
     """
     Log an AtomMsg.
 
     :param msg AtomMsg: The message to send to logger
     :param logger: The logger function. Default :py:func:rospy.loginfo
+    :param color: function to colororize a string
+    :type color: function
     """
     dmsg = {varslot.name: varslot for varslot in msg.varslots}
     atom_name = dmsg['type'].val
-    logger("----------")
-    logger("Translated Atom with Type {}".format(atom_name))
-    logger("Slots of {} Atom are:".format(atom_name))
+    logger(color("----------"))
+    logger(color("Translated Atom with Type: ") + "{}".format(atom_name))
+    logger(color("The Slots of the Atom are:"))
     for k in dmsg.keys():
         if k not in def_slots:
             log_varslot(dmsg[k], logger=logger)
-    logger("----------")
+    logger(color("----------"))
 
 
-def log_varslot(varslot, logger=loginfo):
+def log_varslot(varslot, logger=loginfo, color=blue):
     """
     Log a VarSlot msg.
 
     :param varslot VarSlot: The message to send to logger
     :param logger: The logger function. Default :py:func:rospy.loginfo
+    :param color: function to colororize a string
+    :type color: function
     """
-    logger("Name: {} -- Val: {} -- Type: {}"
-           .format(varslot.name, varslot.val, varslot.type))
+    logger("{} {} -- {} {} -- {} {}"
+           .format(color("Name:"), varslot.name,
+                   color("Val:"), varslot.val,
+                   color("Type:"), varslot.type))
 
 # TTS UTILS ###################################################################
 TTS_ENGINES = ('novoice', 'festival', 'google', 'loquendo', 'microsoft',
@@ -111,8 +127,15 @@ get_nouns = partial(get_tagged, tag='NN')
 get_verbs = partial(get_tagged, tag='VB')
 
 
-# ASR PARSING UTILS ###########################################################
+def get_first_noun(sentence):
+    """Return the first noun that is found in a sentence."""
+    s = parse(sentence)
+    tokens = tokenize(s.split(' '))
+    nouns = zip(*get_nouns(tokens))[0]
+    return nouns[0]
 
+
+# ASR PARSING UTILS ###########################################################
 
 def get_nouns_from_asr(asr_msg):
     """Get all nouns from an ASR message."""
