354362286953ff6041545bcc9a9fde509ff63002
monarch <victor.gonzalez.pacheco@gmail.com>
2015-04-16 20:26:48 +0200

    (m) Improving docstring formatting (PEP257: D205)

--- ocular_interaction/nodes/aggregators/asr_command_parser.py
+++ ocular_interaction/nodes/aggregators/asr_command_parser.py
@@ -19,10 +19,10 @@
 # disponible en <URL a la LASR_UC3Mv1.0>.
 
 """
+Node that parses the asr output and converts it to certain commands.
+
 :author: Victor Gonzalez Pacheco
 :maintainer: Victor Gonzalez Pacheco
-
-Node that parses the asr output and converts it to certain commands.
 """
 import roslib
 roslib.load_manifest('ocular_interaction')
