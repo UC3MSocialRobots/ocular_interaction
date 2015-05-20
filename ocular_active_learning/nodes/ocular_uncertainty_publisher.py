1f294cf14f9cac4b8aaf64b564bae755bb9f7524
Víctor González Pacheco <victor.gonzalez.pacheco@gmail.com>
2015-05-20 20:07:08 +0200

    [FIX] uncertainty_publisher
    Bug corrections that prevented the node to publish margin and entropy
    Massive refactoring and renaming of its pipes and calculator functions

--- ocular_active_learning/nodes/ocular_uncertainty_publisher.py
+++ ocular_active_learning/nodes/ocular_uncertainty_publisher.py
@@ -16,7 +16,7 @@
 # disponible en <URL a la LASR_UC3Mv1.0>.
 
 """
-Node that performs data processing on named_predictions.
+Node that performs data processing on NamedPredictions.
 
 It publishes the estimated value of the received predictions,
 and the entropy and margin of these predictions.
@@ -31,7 +31,6 @@ roslib.load_manifest('ocular_active_learning')
 import rospy
 from rospy_utils import coroutines as co
 
-from ocular_interaction import utils
 from ocular_active_learning import al_utils as alu
 
 # from std_msgs.msg import Float32
@@ -39,23 +38,33 @@ from ocular_msgs.msg import (NamedPredictions, Prediction)
 from ocular_msgs.msg import UncertaintyMetric as Uncertainty
 
 
-def _make_entropy_msg(predictions):
+def calc_entropy(predictions):
     """Make an Uncertainty msg with entropy values of the input predictions."""
     return Uncertainty(name='entropy',
                        rgb=alu.entropy(predictions.rgb),
                        pcloud=alu.entropy(predictions.pcloud))
 
 
-def _make_margin_msg(predictions):
+def calc_margin(predictions):
     """Make an Uncertainty msg with margin values of the input predictions."""
     return Uncertainty(name='margin',
-                       rgb=alu.numerize(alu.margin(predictions.rgb)),
-                       pcloud=alu.numerize(alu.margin(predictions.pcloud)))
+                       rgb=alu.margin(alu.numerize(predictions.rgb)),
+                       pcloud=alu.margin(alu.numerize(predictions.pcloud)))
 
 
-def _make_prediction_msg(prediction_fields):
-    """Make a prediction message from a tuple containing the msg fields."""
-    combined, rgb, pcloud = prediction_fields
+def estimate(predictions):
+    """
+    Estimate of the matched object from the last named predictions.
+
+    Args:
+      predictions (ocular_msgs/NamedPredictions):
+        The predictions wrapped in a message
+
+    Returns:
+        ocular_msgs/Prediction: The predicted object wrapped in a message.
+    """
+    combined, rgb, pcloud = alu.estimate(predictions.rgb, predictions.pcloud)
+    rospy.logwarn("Fields: {} {} {}".format(combined, rgb, pcloud))
     return Prediction(combined=combined, rgb=rgb, pcloud=pcloud)
 
 
@@ -64,31 +73,28 @@ def _init_node(node_name):
     rospy.init_node(node_name)
     rospy.loginfo("Initializing {} Node".format(rospy.get_name()))
 
-
-def _log_predictions(predictions, logger=rospy.loginfo):
-    """Log predictions msg to logger."""
-    logger("Predictions RGB: {}".format(utils.blue(str(predictions.rgb))))
-    logger("Predictions PCloud: {}".format(utils.blue(str(predictions.pcloud))))
-
-entropy_pipe = co.pipe([co.mapper(_make_entropy_msg),
+entropy_pipe = co.pipe([co.mapper(calc_entropy),
                         co.publisher('predictions_entropy', Uncertainty)])
+"""Pipe that gets NamedPredictions and publishes their entropy."""
 
-margin_pipe = co.pipe([co.mapper(_make_margin_msg),
+margin_pipe = co.pipe([co.mapper(calc_margin),
                        co.publisher('predictions_margin', Uncertainty)])
+"""Pipe that gets NamedPredictions and publishes their margin."""
 
-estimator_pipe = co.pipe([co.mapper(alu.estimate),
-                          co.mapper(_make_prediction_msg),
+estimator_pipe = co.pipe([co.mapper(estimate),
                           co.publisher('predicted_object', Prediction)])
+"""Pipe that gets NamedPredictions and publishes the estimated object."""
 
-metrics_pipes = co.splitter(entropy_pipe, margin_pipe, estimator_pipe)
+pipes = co.splitter(entropy_pipe, margin_pipe, estimator_pipe)
+"""Single entry point to entropy_pipe, margin_pipe, and estimator_pipe."""
 
-_DEFAULT_NAME = 'uncertainty_puglisher'
+_DEFAULT_NAME = 'uncertainty_publisher'
+"""Default name of the node."""
 
 if __name__ == '__main__':
     try:
         _init_node(_DEFAULT_NAME)
-        # pipe = co.pipe([co.mapper(alu.numerize), metrics_pipes])
-        co.PipedSubscriber('named_predictions', NamedPredictions, metrics_pipes)
+        co.PipedSubscriber('named_predictions', NamedPredictions, pipes)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
