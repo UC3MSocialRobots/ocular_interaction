55bab4ba72ee4060fecff1731e478c0f526fd92c
Víctor González Pacheco <victor.gonzalez.pacheco@gmail.com>
2015-06-25 20:11:36 +0200

    [DOC] using `readthedocs` sphinx theme

--- ocular_active_learning/doc/conf.py
+++ ocular_active_learning/doc/conf.py
@@ -76,7 +76,7 @@ release = '0.1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['.build']
+exclude_patterns = ['.build', 'ocular_active_learning.msg']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -103,7 +103,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'nature'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -112,6 +115,7 @@ html_theme = 'default'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
